932f0134b2b4f33f468fbe827bc871f05d6ac0ec
Sreethaj Ragi <sreethaj.ragi@mercuresolution.com>
2026-02-13 11:56:26 +0530

    api change

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || "https://api.assessmentcenter.mercuresolution.com";
+const API_URL = import.meta.env.VITE_API_URL || "https://api.assessmentcenter.mercuresolution.com/api";
 const api = axios.create({
     baseURL: API_URL,
     headers: {
